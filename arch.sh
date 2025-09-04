cc3424adc151585794564ca71a10ff09368e7624
bitarc <yljos@outlook.com>
2025-09-04 14:56:32 +0800

    mpv

--- arch.sh
+++ arch.sh
@@ -145,7 +145,7 @@ install_packages() {
         fuzzel swww dunst foot polkit \
         nfs-utils fastfetch btop pipewire pipewire-jack pipewire-alsa pipewire-pulse pavucontrol \
         fcitx5 fcitx5-rime fcitx5-configtool rsync ntfs-3g curl p7zip ranger reflector libnotify openssh \
-        mpd mpc freerdp xf86-video-intel libva libva-intel-driver intel-media-driver vlc arp-scan unzip \
+        mpd mpc freerdp xf86-video-intel libva libva-intel-driver intel-media-driver mpv arp-scan unzip \
         ttf-liberation terminus-font fontconfig wakeonlan noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji \
         sox libva-utils telegram-desktop ufw bc firejail nodejs stow firefox
 
